6236fdef72272e03c47af0333d1747977b06feca
Fernando Montero <fernando.montero.perez@gmail.com>
2020-06-18 20:44:28 +0100

    Moved skills to its own section

--- src/.vuepress/theme/global-components/HomePage.vue
+++ src/.vuepress/theme/global-components/HomePage.vue
@@ -35,9 +35,9 @@
     </div>
 
     <div class="sectionAlt text-paragraph">
-      <div class="w-full max-w-screen-lg p-8 mx-auto">
+      <div class="w-full max-w-screen-md p-8 mx-auto">
         <div class="flex flex-wrap">
-          <div class="w-full md:w-2/3 md:pr-4">
+          <div class="w-full">
             <h2>About Me</h2>
             <p>I am a Full-Stack Developer from the Canary Islands, Spain.</p>
 
@@ -59,26 +59,90 @@
               <i class="text-5xl devicon-javascript-plain"></i>
             </div>
           </div>
-          <div class="w-full md:w-1/3">
-            <h2>My Skills</h2>
-            <ul>
-              <li>Vue / Vuepress</li>
-              <li>React</li>
-              <li>JavaScript ES6</li>
-              <li>CSS / SCSS / Tailwind</li>
-              <li>RESTful API Services</li>
-              <li>SQL / MySql</li>
-              <li>MongoDb / Mongoose</li>
-              <li>Java</li>
-            </ul>
-          </div>
         </div>
       </div>
     </div>
     <div class="section text-paragraph">
+      <div class="w-full max-w-screen-lg p-8 mx-auto flex flex-wrap">
+        <div class=" w-full ">
+          <h2>My Skills</h2>
+        </div>
+        <div class=" w-full md:w-1/2 px-2">
+          <div class="card flex">
+            <div>
+              <i class="text-5xl devicon-react-plain"></i>
+            </div>
+            <div class="pl-4">
+              <h3>Front-end</h3>
+              <div class="flex">
+                <pill-item class="rounded-full font-normal">react</pill-item>
+                <pill-item class="rounded-full font-normal">vue</pill-item>
+                <pill-item class="rounded-full font-normal">parcel</pill-item>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class=" w-full md:w-1/2 px-2">
+          <div class="card flex">
+            <div>
+              <i class="text-5xl devicon-nodejs-plain"></i>
+            </div>
+            <div class="pl-4">
+              <h3>Back-end</h3>
+              <div class="flex">
+                <pill-item class="rounded-full font-normal">REST</pill-item>
+                <pill-item class="rounded-full font-normal">node.js</pill-item>
+                <pill-item class="rounded-full font-normal">mongo</pill-item>
+                <pill-item class="rounded-full font-normal">sql</pill-item>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class=" w-full md:w-1/2 px-2">
+          <div class="card flex">
+            <div>
+              <i class="text-5xl devicon-devicon-plain"></i>
+            </div>
+            <div class="pl-4">
+              <h3>Analysis</h3>
+              <div class="flex flex-wrap">
+                <pill-item class="rounded-full font-normal">
+                  google analytics
+                </pill-item>
+                <pill-item class="rounded-full font-normal">
+                  search console
+                </pill-item>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class=" w-full md:w-1/2 px-2">
+          <div class="card flex">
+            <div>
+              <i class="text-5xl devicon-photoshop-plain"></i>
+            </div>
+            <div class="pl-4">
+              <h3>Design</h3>
+              <div class="flex flex-wrap">
+                <pill-item class="rounded-full font-normal">
+                  photoshop
+                </pill-item>
+                <pill-item class="rounded-full font-normal">
+                  illustrator
+                </pill-item>
+                <pill-item class="rounded-full font-normal">
+                  figma
+                </pill-item>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="w-full max-w-screen-lg p-8 mx-auto">
         <h2>Timeline</h2>
-        <timeline></timeline>
+        <timeline />
       </div>
     </div>
   </div>
@@ -87,9 +151,10 @@
 <script>
 import FaIcon from "./Atoms/fa-icon";
 import Timeline from "./Molecules/Timeline";
+import PillItem from "./Atoms/pill-item";
 export default {
   name: "HomePage",
-  components: { Timeline, FaIcon },
+  components: { Timeline, FaIcon, PillItem },
 };
 </script>
 
